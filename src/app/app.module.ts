2fdb94aa1b4515f77ffeaf6b4581c665f3409f13
Sergio Matteoda <sergio.matteoda@globant.com>
2018-08-01 16:23:46 -0300

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TutorialsComponent } from './tutorials/tutorials.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TutorialsComponent
   ],
   imports: [
     BrowserModule,
